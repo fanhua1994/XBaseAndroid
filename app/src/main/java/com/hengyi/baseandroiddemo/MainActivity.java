1c04a260415afa8cbd0d3e321c332d08ebaaec5b
fanhua1994 <18383219470@163.com>
2017-10-11 11:13:53 +0800

    update

--- app/src/main/java/com/hengyi/baseandroiddemo/MainActivity.java
+++ app/src/main/java/com/hengyi/baseandroiddemo/MainActivity.java
@@ -4,20 +4,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.devspark.appmsg.AppMsg;
-import com.hengyi.baseandroidcore.base.BaseActivity;
 import com.hengyi.baseandroidcore.base.WebEngineActivity;
-import com.hengyi.baseandroidcore.dialog.CustomAlertDialog;
-import com.hengyi.baseandroidcore.dialog.CustomConfirmDialog;
 import com.hengyi.baseandroidcore.utils.ActivityStack;
-import com.hengyi.baseandroidcore.utils.ConfigUtils;
-import com.hengyi.baseandroidcore.utils.ProjectUtils;
-import com.hengyi.baseandroidcore.utils.TimerUtils;
-import com.hengyi.baseandroidcore.utilscode.LogUtils;
 import com.hengyi.baseandroidcore.weight.EaseTitleBar;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends MyBaseActivity {
