42693a7bd567133124171354a39378e9ccf6d33f
fanhua1994 <18383219470@163.com>
2017-10-27 11:02:53 +0800

    加入BaseFragment

--- baseandroidcore/src/main/java/com/hengyi/baseandroidcore/base/BaseFragment.java
+++ baseandroidcore/src/main/java/com/hengyi/baseandroidcore/base/BaseFragment.java
@@ -1,11 +1,76 @@
 package com.hengyi.baseandroidcore.base;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Administrator on 2017/10/10.
  */
+public abstract class BaseFragment extends Fragment {
 
-public class BaseFragment extends Fragment {
+    public Activity mActivity;
+
+    /**
+     * 此方法可以得到上下文对象
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+    /*
+     * 返回一个需要展示的View
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mActivity = getActivity();
+        View view = initView(inflater);
+        initFindViewById(view);
+
+        return view;
+    }
+
+
+
+    /**
+     * 子类可以复写此方法初始化事件
+     */
+    protected  void initEvent(){
+
+    }
+
+    /*
+     * 当Activity初始化之后可以在这里进行一些数据的初始化操作
+     */
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+        initEvent();
+    }
+
+    /**
+     * 子类实现此抽象方法返回View进行展示
+     *
+     * @return
+     */
+    public abstract View initView(LayoutInflater inflater);
+
+    /**
+     * 初始化控件
+     */
+    protected abstract void initFindViewById(View view);
+
+    /**
+     * 子类在此方法中实现数据的初始化
+     */
+    public  abstract void initData() ;
 
 }
