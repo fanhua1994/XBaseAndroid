0513dd0bb68e4ea197db2c2bcc6352362e77fff4
fanhua1994 <18383219470@163.com>
2017-11-22 09:47:14 +0800

    删除刷新组件

--- app/src/main/java/com/hengyi/baseandroiddemo/MainActivity.java
+++ app/src/main/java/com/hengyi/baseandroiddemo/MainActivity.java
@@ -9,22 +9,13 @@ import android.widget.Button;
 import com.hengyi.baseandroidcore.app.AppUpdateManager;
 import com.hengyi.baseandroidcore.app.UpdateBean;
 import com.hengyi.baseandroidcore.base.XbaseWebActivity;
-import com.hengyi.baseandroidcore.database.DatabaseHelper;
-import com.hengyi.baseandroidcore.database.DatabaseVersionChangeListener;
-import com.hengyi.baseandroidcore.dialog.CustomConfirmDialog;
-import com.hengyi.baseandroidcore.utils.CountDownUtils;
 import com.hengyi.baseandroidcore.utils.NotifacationUtils;
 import com.hengyi.baseandroidcore.utilscode.PermissionUtils;
-import com.hengyi.baseandroidcore.validation.ValidMsg;
-import com.hengyi.baseandroidcore.validation.Validation;
 import com.hengyi.baseandroidcore.weight.EaseTitleBar;
 import com.hengyi.db.Student;
 import com.hengyi.db.StudentDao;
-import com.hengyi.validation.User;
 
 import java.io.File;
-import java.sql.SQLException;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -38,96 +29,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        CustomConfirmDialog confirmDialog = new CustomConfirmDialog(this).builder();
-        confirmDialog.setTitle("请输入您的姓名");
-        confirmDialog.setInputNumber(false);//限制数字
-        confirmDialog.setNegativeButton("取消", new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
-
-        confirmDialog.setPositiveButton("确定",new CustomConfirmDialog.OnPostListener(){
-            @Override
-            public void OnPost(String value) {
-                toast(value);
-            }
-        });
-        confirmDialog.show();
-
-
-
-        showLoadingDialog("正在加载");
-
-        CountDownUtils countDownUtils = new CountDownUtils(10000,1000);
-        countDownUtils.start(new CountDownUtils.setOnCountDownListener() {
-            @Override
-            public void onTick(int second) {
-
-            }
-
-            @Override
-            public void onFinish() {
-                closeLoadingDialog();
-            }
-        });
-
-
-
-        //扫描表
-        DatabaseHelper.addTable(Student.class);
-        DatabaseHelper.setDatabase("easy",2);
-
-        DatabaseHelper.getInstance(this).setDatabaseVersionChangeListener(new DatabaseVersionChangeListener() {
-            @Override
-            public void onChange(int oldVersion, int newVersion) {
-                toast("数据库版本发生变化：老版本:"+oldVersion +" 新版本："+newVersion);
-                if(oldVersion < 2){
-                    try {
-                        DatabaseHelper.getInstance(getContext()).getDao(Student.class).executeRaw("ALTER TABLE 'student' ADD COLUMN sex int");
-                        toast("数据更新成功");
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                        toast("数据更新失败");
-                    }
-                }
-            }
-        });
-
-        toast("当前数据库版本："+DatabaseHelper.getInstance(this).getVersion() +"数据库名："+DatabaseHelper.getInstance(this).getDatabaseName());
-
-        studentDao = new StudentDao(this);
-        toast("学生数量：" + studentDao.count());
-        List<Student> allstudent = studentDao.getAll();
-        if(allstudent == null){
-            toast("数据为空");
-        }else{
-            toast(allstudent.get(0).getName());
-        }
-
-        easeTitleBar.setLeftLayoutClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                User user = new User();
-                user.setAddress("四川");
-                user.setAge(90);
-                user.setName("董志平");
-                user.setSex("44444");
-                user.setMail("dddddd@163.com");
-                user.setIdcard("514199101331343241X");
-
-                ValidMsg msg = Validation.AutoVerifiy(user);
-                //ValidMsg msg = Validation.StringSize(user.getName(), "用户姓名", 2, 10);//仅演示了校验长度，其他的方法请参考Method.java内部。
-
-                if(msg.isPass()){
-                    toast("验证通过");
-                }else{
-                   toast("验证失败："+msg.getMsg());
-                }
-
-            }
-        });
      }
 
     @Override
--- baseandroidcore/src/main/java/com/hengyi/baseandroidcore/utils/GlideImageUtils.java
+++ baseandroidcore/src/main/java/com/hengyi/baseandroidcore/utils/GlideImageUtils.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Looper;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -13,11 +15,16 @@ import android.widget.RelativeLayout;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.cache.ExternalCacheDiskCacheFactory;
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
 import com.bumptech.glide.request.animation.GlideAnimation;
 import com.bumptech.glide.request.target.SimpleTarget;
 import com.hengyi.baseandroidcore.R;
 import com.hengyi.baseandroidcore.tools.GlideBlurTransformation;
 import com.hengyi.baseandroidcore.tools.GlideCircleTransform;
+
+import java.io.File;
+import java.math.BigDecimal;
 
 import jp.wasabeef.glide.transformations.BlurTransformation;
 import jp.wasabeef.glide.transformations.CropCircleTransformation;
@@ -357,5 +364,153 @@ public class GlideImageUtils {
                         RoundedCornersTransformation.CornerType.ALL))// 设置矩形圆角
                 .diskCacheStrategy(DiskCacheStrategy.RESULT)// 缓存修改过的图片
                 .into(imageview);
+    }
+
+    //======================缓存清理=============================
+    /**
+     * 清除图片磁盘缓存
+     */
+    public static void clearImageDiskCache(final Context context) {
+        try {
+            if (Looper.myLooper() == Looper.getMainLooper()) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Glide.get(context).clearDiskCache();
+                    }
+                }).start();
+            } else {
+                Glide.get(context).clearDiskCache();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 清除图片内存缓存
+     */
+    public static void clearImageMemoryCache(Context context) {
+        try {
+            if (Looper.myLooper() == Looper.getMainLooper()) { //只能在主线程执行
+                Glide.get(context).clearMemory();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 清除图片所有缓存
+     */
+    public static void clearImageAllCache(Context context) {
+        clearImageDiskCache(context);
+        clearImageMemoryCache(context);
+        String ImageExternalCatchDir=context.getExternalCacheDir()+ ExternalCacheDiskCacheFactory.DEFAULT_DISK_CACHE_DIR;
+        deleteFolderFile(ImageExternalCatchDir, true);
+    }
+
+    /**
+     * 获取Glide造成的缓存大小
+     *
+     * @return CacheSize
+     */
+    public static String getCacheSize(Context context) {
+        try {
+            return getFormatSize(getFolderSize(new File(context.getCacheDir() + "/"+ InternalCacheDiskCacheFactory.DEFAULT_DISK_CACHE_DIR)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * 获取指定文件夹内所有文件大小的和
+     *
+     * @param file file
+     * @return size
+     * @throws Exception
+     */
+    private static long getFolderSize(File file) throws Exception {
+        long size = 0;
+        try {
+            File[] fileList = file.listFiles();
+            for (File aFileList : fileList) {
+                if (aFileList.isDirectory()) {
+                    size = size + getFolderSize(aFileList);
+                } else {
+                    size = size + aFileList.length();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return size;
+    }
+
+    /**
+     * 删除指定目录下的文件，这里用于缓存的删除
+     *
+     * @param filePath filePath
+     * @param deleteThisPath deleteThisPath
+     */
+    private static void deleteFolderFile(String filePath, boolean deleteThisPath) {
+        if (!TextUtils.isEmpty(filePath)) {
+            try {
+                File file = new File(filePath);
+                if (file.isDirectory()) {
+                    File files[] = file.listFiles();
+                    for (File file1 : files) {
+                        deleteFolderFile(file1.getAbsolutePath(), true);
+                    }
+                }
+                if (deleteThisPath) {
+                    if (!file.isDirectory()) {
+                        file.delete();
+                    } else {
+                        if (file.listFiles().length == 0) {
+                            file.delete();
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 格式化单位
+     *
+     * @param size size
+     * @return size
+     */
+    private static String getFormatSize(double size) {
+
+        double kiloByte = size / 1024;
+        if (kiloByte < 1) {
+            return size + "Byte";
+        }
+
+        double megaByte = kiloByte / 1024;
+        if (megaByte < 1) {
+            BigDecimal result1 = new BigDecimal(Double.toString(kiloByte));
+            return result1.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "KB";
+        }
+
+        double gigaByte = megaByte / 1024;
+        if (gigaByte < 1) {
+            BigDecimal result2 = new BigDecimal(Double.toString(megaByte));
+            return result2.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "MB";
+        }
+
+        double teraBytes = gigaByte / 1024;
+        if (teraBytes < 1) {
+            BigDecimal result3 = new BigDecimal(Double.toString(gigaByte));
+            return result3.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "GB";
+        }
+        BigDecimal result4 = new BigDecimal(teraBytes);
+
+        return result4.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "TB";
     }
 }
