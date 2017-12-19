23e7d50947aab3fb0382e16a8531846c12f7faee
fanhua1994 <18383219470@163.com>
2017-12-19 09:21:03 +0800

    预发布1.0.4

--- LOG.md
+++ LOG.md
@@ -13,5 +13,6 @@
 
 ### 20171218（版本1.0.4）【未发布】
 + ActivityUtils.kill()会销毁全部Activity并杀死进程。不用再调用ActivityStack.getInstance().clearAllActivity();方法。
++ ActivityUtils新增Flag,Action参数。
 + 预计将引入下拉刷新上拉加载库
 + 引入内存泄漏检测工具
--- baseandroidcore/src/main/java/com/hengyi/baseandroidcore/utils/ActivityUtils.java
+++ baseandroidcore/src/main/java/com/hengyi/baseandroidcore/utils/ActivityUtils.java
@@ -70,4 +70,103 @@ public class ActivityUtils {
         }
         context.startActivityForResult(intent, result);
     }
+
+    //======================引入Flag===============================
+    //界面跳转 不带参数
+    public static void StartActivity(Context context,Class cla,int flag) {
+        intent = null;
+        intent = new Intent(context, cla);
+        intent.addFlags(flag);
+        context.startActivity(intent);
+    }
+
+    //带自定义参数
+    public  static void StartActivity(Context context,Class cla,int flag, String[] names, Object... param) {
+        intent = null;
+        intent = new Intent(context, cla);
+        intent.addFlags(flag);
+        for (int i = 0; i < param.length; i++) {
+            if (param[i].getClass().equals(Integer.class)) {
+                intent.putExtra(names[i], (Integer) param[i]);
+            } else if (param[i].getClass().equals(String.class)) {
+                intent.putExtra(names[i], (String) param[i]);
+            } else if (param[i].getClass().equals(Boolean.class)) {
+                intent.putExtra(names[i], (Boolean) param[i]);
+            } else if (param[i].getClass().equals(Float.class)) {
+                intent.putExtra(names[i], (Float) param[i]);
+            } else if (param[i].getClass().equals(Double.class)) {
+                intent.putExtra(names[i], (Double) param[i]);
+            }
+        }
+        context.startActivity(intent);
+    }
+
+    //带自定义参数  带回调
+    public static void StartActivity(Activity context, Class cla, int flag,int result, String[] names, Object... param) {
+        intent = null;
+        intent = new Intent(context,cla);
+        intent.addFlags(flag);
+        for (int i = 0; i < param.length; i++) {
+            if (param[i].getClass().equals(Integer.class)) {
+                intent.putExtra(names[i], (Integer) param[i]);
+            } else if (param[i].getClass().equals(String.class)) {
+                intent.putExtra(names[i], (String) param[i]);
+            } else if (param[i].getClass().equals(Boolean.class)) {
+                intent.putExtra(names[i], (Boolean) param[i]);
+            } else if (param[i].getClass().equals(Float.class)) {
+                intent.putExtra(names[i], (Float) param[i]);
+            } else if (param[i].getClass().equals(Double.class)) {
+                intent.putExtra(names[i], (Double) param[i]);
+            }
+        }
+        context.startActivityForResult(intent, result);
+    }
+
+    //=================引入Action=====================
+    //界面跳转 不带参数
+    public static void StartActivity(Context context,String action) {
+        intent = null;
+        intent = new Intent(action);
+        context.startActivity(intent);
+    }
+
+    //带自定义参数
+    public  static void StartActivity(Context context,String action, String[] names, Object... param) {
+        intent = null;
+        intent = new Intent(action);
+        for (int i = 0; i < param.length; i++) {
+            if (param[i].getClass().equals(Integer.class)) {
+                intent.putExtra(names[i], (Integer) param[i]);
+            } else if (param[i].getClass().equals(String.class)) {
+                intent.putExtra(names[i], (String) param[i]);
+            } else if (param[i].getClass().equals(Boolean.class)) {
+                intent.putExtra(names[i], (Boolean) param[i]);
+            } else if (param[i].getClass().equals(Float.class)) {
+                intent.putExtra(names[i], (Float) param[i]);
+            } else if (param[i].getClass().equals(Double.class)) {
+                intent.putExtra(names[i], (Double) param[i]);
+            }
+        }
+        context.startActivity(intent);
+    }
+
+    //带自定义参数  带回调
+    public static void StartActivity(Activity context, String action, int result, String[] names, Object... param) {
+        intent = null;
+        intent = new Intent(action);
+        for (int i = 0; i < param.length; i++) {
+            if (param[i].getClass().equals(Integer.class)) {
+                intent.putExtra(names[i], (Integer) param[i]);
+            } else if (param[i].getClass().equals(String.class)) {
+                intent.putExtra(names[i], (String) param[i]);
+            } else if (param[i].getClass().equals(Boolean.class)) {
+                intent.putExtra(names[i], (Boolean) param[i]);
+            } else if (param[i].getClass().equals(Float.class)) {
+                intent.putExtra(names[i], (Float) param[i]);
+            } else if (param[i].getClass().equals(Double.class)) {
+                intent.putExtra(names[i], (Double) param[i]);
+            }
+        }
+        context.startActivityForResult(intent, result);
+    }
 }
