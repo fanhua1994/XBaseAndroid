18f9a961b5a32c533baf54ef9b82e6fe90ed449a
fanhua1994 <18383219470@163.com>
2017-12-26 17:29:33 +0800

    更新系统工具

--- app/src/main/java/com/hengyi/baseandroiddemo/MainActivity.java
+++ app/src/main/java/com/hengyi/baseandroiddemo/MainActivity.java
@@ -16,6 +16,7 @@ import com.hengyi.baseandroidcore.thread.HandlerExecutorPool;
 import com.hengyi.baseandroidcore.utils.ActivityUtils;
 import com.hengyi.baseandroidcore.utils.ColorUtils;
 import com.hengyi.baseandroidcore.utils.NotifacationUtils;
+import com.hengyi.baseandroidcore.utils.SystemUtils;
 import com.hengyi.baseandroidcore.utilscode.PermissionUtils;
 import com.hengyi.baseandroidcore.weight.EaseTitleBar;
 
@@ -40,7 +41,13 @@ public class MainActivity extends BaseActivity{
         super.onCreate(savedInstanceState);
         StatusBarCompat.setStatusBarColor(this, Color.parseColor(ColorUtils.changeColor(this,R.color.main_color)));
         EventBus.getDefault().register(this);
-     }
+
+        try {
+            toast(SystemUtils.getClientID(this));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public int setContentView() {
--- baseandroidcore/src/main/java/com/hengyi/baseandroidcore/utils/SystemUtils.java
+++ baseandroidcore/src/main/java/com/hengyi/baseandroidcore/utils/SystemUtils.java
@@ -4,9 +4,12 @@ package com.hengyi.baseandroidcore.utils;
  * Created by Administrator on 2017/9/27.
  */
 
-import android.app.Activity;
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.telephony.TelephonyManager;
 
 import java.util.Locale;
@@ -28,7 +31,7 @@ public class SystemUtils {
     /**
      * 获取当前手机系统版本号
      *
-     * @return  系统版本号
+     * @return 系统版本号
      */
     public static String getSystemVersion() {
         return Build.VERSION.RELEASE;
@@ -37,7 +40,7 @@ public class SystemUtils {
     /**
      * 获取手机型号
      *
-     * @return  手机型号
+     * @return 手机型号
      */
     public static String getSystemModel() {
         return Build.MODEL;
@@ -46,22 +49,32 @@ public class SystemUtils {
     /**
      * 获取手机厂商
      *
-     * @return  手机厂商
+     * @return 手机厂商
      */
     public static String getDeviceBrand() {
         return Build.BRAND;
     }
 
-    /**
-     * 获取手机IMEI(需要“android.permission.READ_PHONE_STATE”权限)
-     *
-     * @return  手机IMEI
-     */
-    public static String getIMEI(Context ctx) {
-        TelephonyManager tm = (TelephonyManager) ctx.getSystemService(Activity.TELEPHONY_SERVICE);
-        if (tm != null) {
-            return tm.getDeviceId();
+    public static String getIMEI(Context context) throws Exception {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            throw new Exception("请注意申请权限");
         }
-        return null;
+        String imei = tm.getImei();
+        if(imei == null){
+            throw new Exception("出现未知错误");
+        }
+
+        return imei;
+    }
+
+    /**
+     * 获取唯一ID
+     * @param context
+     * @return
+     * @throws Exception
+     */
+    public static String getClientID(Context context) throws Exception {
+        return MD5.get(getIMEI(context));
     }
 }
