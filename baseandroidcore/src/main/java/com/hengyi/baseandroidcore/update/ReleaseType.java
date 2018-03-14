c4a358854c80e7e594a22fe051b3690c45f155dd
繁华 <18383219470@163.com>
2018-03-14 17:13:03 +0800

    更新 发布代码

--- baseandroidcore/src/main/java/com/hengyi/baseandroidcore/update/ReleaseType.java
+++ baseandroidcore/src/main/java/com/hengyi/baseandroidcore/update/ReleaseType.java
@@ -4,7 +4,7 @@ package com.hengyi.baseandroidcore.update;
  * Created by ZW-2 on 2018/3/1.
  */
 
-public class BuildType {
+public class ReleaseType {
     public static final int RELEASE = 1;
     public static final int DEBUG = 2;
 }
