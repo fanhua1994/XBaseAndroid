0a994580476dd2c39e9cf5c92b7473ae28f3965a
繁华 <18383219470@163.com>
2018-09-09 15:23:59 +0800

    移除recycleview万能适配器

--- baseandroidcore/src/main/java/com/hengyi/baseandroidcore/adapter/entity/MultiItemEntity.java
+++
@@ -1,8 +0,0 @@
-package com.hengyi.baseandroidcore.adapter.entity;
-
-/**
- * https://github.com/CymChad/BaseRecyclerViewAdapterHelper
- */
-public interface MultiItemEntity {
-    int getItemType();
-}
